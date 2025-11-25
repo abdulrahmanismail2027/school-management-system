1416ec6402a7503c605d5dbbbbb9609c32c019a0
abdulrahmanismail2027 <es-abdelrahman.ismail2027@alexu.edu.eg>
2025-11-25 19:22:56 +0200

    Initial commit

---
+++ backend/src/main/java/com/faithcentre/sms/SchoolManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.faithcentre.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemApplication.class, args);
+	}
+
+}
